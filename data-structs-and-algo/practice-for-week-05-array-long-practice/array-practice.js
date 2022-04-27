3ff8135b431f4a35a2b5aa6efc338d0ec4e3b4de
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-28 07:50:54 +0800

    completed exercise

--- data-structs-and-algo/practice-for-week-05-array-long-practice/array-practice.js
+++ data-structs-and-algo/practice-for-week-05-array-long-practice/array-practice.js
@@ -1,38 +1,113 @@
 const findMinimum = arr => {
+  let min = undefined;
 
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    let currNum = arr[i];
+    if (min === undefined || currNum < min) {
+      min = currNum;
+    }
+  }
 
+  return min;
 };
+
+// Time: O(n)
+// Space: O(1)
 
 const runningSum = arr => {
+  const newArr = [];
+  let runningSum = 0;
 
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    runningSum += num;
+
+    newArr.push(runningSum);
+  }
+
+  return newArr
 };
+
+// Time: O(n)
+// Space: O(n)
 
 const evenNumOfChars = arr => {
+  let count = 0;
 
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    let string = arr[i];
+
+    if (string.length % 2 === 0) {
+      count++
+    }
+  }
+
+  return count;
 };
+
+// Time: O(n)
+// Space: O(1)
 
 const smallerThanCurr = arr => {
+  const counts = [];
 
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    const curr = arr[i];
 
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < curr) {
+        count++;
+      }
+    }
+
+    counts.push(count);
+  }
+
+  return counts;
 };
+
+// Time: O(n^2)
+// Space: O(n)
 
 const twoSum = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      let num2 = arr[j];
 
-  // Your code here
+      if (num1 + num2 === target) return true;
+    }
+  }
+
+  return false;
 };
+
+// Time: O(n^2)
+// Space: O(1)
 
 const secondLargest = arr => {
+  const sorted = arr.sort((a, b) => a - b);
 
-  // Your code here
+  return sorted[sorted.length - 2];
 };
 
-const shuffle = (arr) => {
+// Time: O(logn)
+// Space: O(n)
 
-  // Your code here
+const shuffle = (arr) => {
+  const newArr = [];
+
+  while (newArr.length < arr.length) {
+    let randIdx = Math.floor(Math.random() * arr.length);
+    let el = arr[randIdx];
+
+    if (!newArr.includes(el)) {
+      newArr.push(el)
+    }
+  }
+
+  return newArr;
 };
 
 
