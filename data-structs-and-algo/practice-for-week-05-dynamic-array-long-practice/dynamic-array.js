1891ce67415c620e0a5e452cd4a9c8cf20e4a023
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-28 17:15:12 +0800

    completed exercise

--- data-structs-and-algo/practice-for-week-05-dynamic-array-long-practice/dynamic-array.js
+++ data-structs-and-algo/practice-for-week-05-dynamic-array-long-practice/dynamic-array.js
@@ -1,44 +1,75 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
-
-    // Your code here
+  constructor(defaultSize = 4) {
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
+    if (this.length === this.capacity) {
+      this.resize();
+    }
 
-    // Your code here
+    this.data[this.length] = val;
+    this.length++;
   }
 
 
   pop() {
-
-    // Your code here
+    this.length--;
+    return this.data[this.length];
   }
 
   shift() {
+    const firsEl = this.data[0];
 
-    // Your code here
+    for (let i = 1; i < this.length; i++) {
+      this.data[i - 1] = this.data[i];
+    }
+
+    this.length--;
+    return firsEl;
   }
 
   unshift(val) {
+    if (this.length === this.capacity) {
+      this.resize();
+    }
 
-    // Your code here
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf (val) {
+    for (let i = 0; i < this.length; i++) {
+      const currEl = this.data[i];
 
-    // Your code here
+      if (currEl === val) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 
   resize () {
+    this.capacity *= 2;
+    const newData = new Array(this.capacity);
 
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+
+    this.data = newData;
   }
 
 }
