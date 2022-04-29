0c156e132d79ee6d7ec96980069dcda5960eeb41
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-29 14:03:13 +0800

    finished part 4 of the exercise

--- data-structs-and-algo/practice-for-week-05-hash-tables-practice/hash-table.js
+++ data-structs-and-algo/practice-for-week-05-hash-tables-practice/hash-table.js
@@ -53,11 +53,36 @@ class HashTable {
   }
 
   insert(key, value) {
-    // Your code here
+    const bucketIndex = this.hashMod(key);
+    const bucket = this.data[bucketIndex];
+    const newPair = new KeyValuePair(key, value);
+
+    // if the bucket isn't occupied, just insert the newPair into the bucket
+    // then return
+    if (!bucket) {
+      this.data[bucketIndex] = newPair;
+      this.count += 1;
+      return;
+    }
+
+    // traverse the bucket list to check if the key already exist
+    // if it does, just update the value then return
+    let currPair = bucket;
+    while (currPair) {
+      if (currPair.key === key) {
+        currPair.value = value;
+        return;
+      }
+
+      currPair = currPair.next;
+    }
+
+    // if any of the above conditions isn't true, add the newPair to the 'head'
+    // of the bucket that's already occupied
+    newPair.next = bucket;
+    this.data[bucketIndex] = newPair;
+    this.count += 1;
   }
-
-
-
 }
 
 
