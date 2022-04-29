58dc21d3554c1515ca0c6c411d121a0fbe61b0b4
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-29 16:00:21 +0800

    completed phase 2

--- data-structs-and-algo/practice-for-week-05-hash-tables-long-practice/phases/02-practice-problems.js
+++ data-structs-and-algo/practice-for-week-05-hash-tables-long-practice/phases/02-practice-problems.js
@@ -1,25 +1,87 @@
 function anagrams(str1, str2) {
-  // Your code here
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const str1CharsCounts = countChars(str1);
+  const str2CharsCounts = countChars(str2);
+
+  for (let char in str1CharsCounts) {
+    if (str1CharsCounts[char] !== str2CharsCounts[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function countChars(str) {
+  const counts = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (char in counts) {
+      counts[char] += 1;
+    } else {
+      counts[char] = 1;
+    }
+  }
+
+  return counts;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  const hashSet1 = new Set(arr1);
+  const hashSet2 = new Set(arr2);
+  const common = [];
+
+  hashSet1.forEach(el => {
+    if (hashSet2.has(el)) {
+      common.push(el)
+    }
+  })
+
+  return common;
 }
 
 
 function duplicate(arr) {
-  // Your code here
+  const visited = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+
+    if (visited.has(el)) {
+      return el;
+    }
+
+    visited.add(el);
+  }
 }
 
 
 function twoSum(nums, target) {
-  // Your code here
+  const numsSet = new Set(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+
+    if (numsSet.has(diff) && diff !== nums[i]) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 
 function wordPattern(pattern, strings) {
-  // Your code here
+  const patternSet = new Set(pattern.split(""));
+	const stringsSet = new Set(strings);
+
+	return patternSet.size === stringsSet.size;
 }
 
 
