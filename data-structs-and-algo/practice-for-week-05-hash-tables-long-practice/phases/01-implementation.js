56f395ab3c8e0d438274ea6e07ab94ed2d25d3d0
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-29 15:26:36 +0800

    implemente read, resize and insert

--- data-structs-and-algo/practice-for-week-05-hash-tables-long-practice/phases/01-implementation.js
+++ data-structs-and-algo/practice-for-week-05-hash-tables-long-practice/phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
@@ -30,24 +31,77 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    const bucketIndex = this.hashMod(key);
+    const bucket = this.data[bucketIndex];
+    const newPair = new KeyValuePair(key, value);
+
+    if (!bucket) {
+      this.data[bucketIndex] = newPair;
+      this.count += 1;
+      return;
+    }
+
+    let currPair = bucket;
+    while (currPair) {
+      if (currPair.key === key) {
+        currPair.value = value;
+        return;
+      }
+
+      currPair = currPair.next;
+    }
+
+    newPair.next = bucket;
+    this.data[bucketIndex] = newPair;
+    this.count += 1;
   }
 
 
   read(key) {
-    // Your code here
+    const bucketIndex = this.hashMod(key);
+    let bucket = this.data[bucketIndex];
+
+    if (bucket.key === key) {
+      return bucket.value;
+    }
+
+    let currPair = bucket;
+    while (currPair) {
+      if (currPair.key === key) {
+        return currPair.value;
+      }
+
+      currPair = currPair.next;
+    }
   }
 
 
   resize() {
-    // Your code here
+    const oldData = this.data;
+
+    this.capacity *= 2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+
+    oldData.forEach(datum => {
+      let currPair = datum;
+
+      while (currPair) {
+        this.insert(currPair.key, currPair.value);
+        currPair = currPair.next;
+      }
+    })
+
   }
 
-
   delete(key) {
-    // Your code here
+    // code here
   }
 }
 
+
+const hashTable = new HashTable(4)
+
+console.log(hashTable.hashMod('key1'), hashTable.hashMod('key5'));
 
 module.exports = HashTable;
