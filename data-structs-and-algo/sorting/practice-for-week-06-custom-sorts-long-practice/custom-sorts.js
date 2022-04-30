00dd25f1496fffb461e0fa8deced3ab814f59806
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-30 14:08:33 +0800

    completed exercises

--- data-structs-and-algo/sorting/practice-for-week-06-custom-sorts-long-practice/custom-sorts.js
+++ data-structs-and-algo/sorting/practice-for-week-06-custom-sorts-long-practice/custom-sorts.js
@@ -1,21 +1,58 @@
 function ageSort(users) {
-  // Your code here
+  users.sort((user1, user2) => {
+    return user1.age - user2.age;
+  })
+
+  return users;
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  arr.sort((n1, n2) => {
+    if (n1 % 2 === 0 && n2 % 2 === 1) return 1;
+    if (n1 % 2 === 1 && n2 % 2 === 0) return -1;
+    return n1 - n2;
+  })
+
+  return arr;
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  const newS = s.split('').sort().join('');
+  const newT = t.split('').sort().join('');
+
+  return newS === newT;
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  arr.sort((a, b) => {
+    if (String(a).length > String(b).length) return -1;
+    if (String(a).length < String(b).length) return 1;
+
+    return a - b;
+  })
+
+  return arr;
 }
 
 function frequencySort(arr) {
-  // Your code here
+  const numCounts = arr.reduce((counts, num) => {
+    if (num in counts) {
+      counts[num] += 1;
+    } else {
+      counts[num] = 1;
+    }
+
+    return counts;
+  }, {});
+
+  arr.sort((a, b) => {
+    if (numCounts[a] < numCounts[b]) return -1;
+    if (numCounts[a] > numCounts[b]) return 1;
+
+    return b - a;
+  })
+
+  return arr;
 }
 
 module.exports = [
