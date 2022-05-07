bcabdbc3f219706107e35718a0642b7efccf172a
Alphonzo Escolar <phonz.dev@gmail.com>
2022-05-07 09:13:26 +0800

    Completed exercises of the timeout project, part1

--- intermediate-javascript/practice-for-week-03-timeout-pt1-practice/problems/02-set-seconds-timeout-args.js
+++ intermediate-javascript/practice-for-week-03-timeout-pt1-practice/problems/02-set-seconds-timeout-args.js
@@ -23,7 +23,9 @@ setSecondsTimeoutArgs(function(arg1, arg2) {
 ***********************************************************************/
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
-  // Your code here
+  setTimeout(() => {
+    cb(...args);
+  }, delayInSeconds * 1000);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
