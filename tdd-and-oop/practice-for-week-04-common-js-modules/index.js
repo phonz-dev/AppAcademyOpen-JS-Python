874fd35e5ab01628d5cc58ecdd6b24de7a530104
Alphonzo Escolar <phonz.dev@gmail.com>
2022-05-07 18:38:23 +0800

    Completed practice project

--- tdd-and-oop/practice-for-week-04-common-js-modules/index.js
+++ tdd-and-oop/practice-for-week-04-common-js-modules/index.js
@@ -1,5 +1,6 @@
-
-// Your code here
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
+const [ msg1, msg2, msg3 ] = require('./messages');
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
