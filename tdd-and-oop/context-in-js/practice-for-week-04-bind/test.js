16696eafa5b069d98fa71c94572c0d62840f70ff
Alphonzo Escolar <phonz.dev@gmail.com>
2022-05-09 10:10:42 +0800

    Completed exercise

--- tdd-and-oop/context-in-js/practice-for-week-04-bind/test.js
+++ tdd-and-oop/context-in-js/practice-for-week-04-bind/test.js
@@ -1,1 +1,7 @@
-// Your code here
+const Employee = require('./employee');
+
+const johnWick = new Employee('John Wick', 'Dog Lover');
+
+setTimeout(johnWick.sayName.bind(johnWick), 2000);
+
+setTimeout(johnWick.sayOccupation.bind(johnWick), 3000);
